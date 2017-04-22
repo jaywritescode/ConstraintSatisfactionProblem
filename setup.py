99a61f518179cd249ae646bce8048753b0092283
Jay Harris <jayharris+git@gmail.com>
2017-04-22 18:15:54 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ConstraintSatisfactionProblem',
+    version='',
+    packages=[''],
+    url='https://github.com/jaywritescode/ConstraintSatisfactionProblem',
+    license='MIT',
+    author='jay harris',
+    author_email='jaywritescode@users.noreply.github.com',
+    description='A Python framework for solving constraint satisfaction problems.'
+)
