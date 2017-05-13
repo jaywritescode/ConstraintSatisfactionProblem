9d67f0eae358d80321c32113acd5f9c812a08480
Jay Harris <jayharris+git@gmail.com>
2017-05-13 17:26:28 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ConstraintSatisfactionProblem',
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['csp'],
     url='https://github.com/jaywritescode/ConstraintSatisfactionProblem',
     license='MIT',
